9da9e90fb178652410f94fe18c615d3b82de7a93
Mingqian Ye <mingqian.ye@33across.com>
2017-02-28 11:42:18 -0800

    work

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject solitaire-core "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Logic for the classic solitaire game"
+  :url "https://github.com/mingqianye/solitaire-core"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
